0f69562e5d788ed3a41965bec1d6f4f7d09241a4
“eriku66” <eriku.hashimoto@gmail.com>
2023-09-17 23:19:55 +0900

    test: 💍 learn and_then()

--- tests/learn202309.rs
+++ tests/learn202309.rs
@@ -7,6 +7,17 @@ fn compare_option() {
     // Option implements Ord if the element type implements Ord.
     // Some is greater than None.
     assert_eq!(a.max(b), Some(4));
-    assert!(a.max(c).is_some());
-    assert!(c.max(None).is_some());
+    assert_eq!(a.max(c), Some(3));
+    assert!(c.max(None).is_none());
+}
+
+#[test]
+fn and_then() {
+    let a = Some(3);
+    let b = None;
+
+    // and_then() return Option<T>.
+    // If and_then() receives None, it returns None.
+    assert_eq!(a.max(b).and_then(|n| Some(n * 3)), Some(9));
+    assert_eq!(b.and_then(|n| Some(n * 3)), None);
 }
